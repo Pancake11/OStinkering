71e5f0525db1bc65ecf1526d14e7e99bc32f8205
maxim payeur <maxim.payeur@gmail.com>
2022-05-01 11:43:31 +0200

    implemented basic memory management kernel API

--- libc/mem.c
+++ libc/mem.c
@@ -29,3 +29,50 @@ uint32_t kmalloc(size_t size, int align, uint32_t *phys_addr) {
 	free_mem_addr += size;
 	return ret;
 }
+
+/*-------------------- memory kernel api ----------------*/
+/*	OSdev wiki meaty skeleton implementation regrouped in one file*/
+
+void* memmove(void* dstptr, const void* srcptr, size_t size) {
+	unsigned char* dst = (unsigned char*) dstptr;
+	const unsigned char* src = (const unsigned char*) srcptr;
+	size_t i;
+	if (dst < src) {
+		for (i = 0; i < size; i++)
+			dst[i] = src[i];
+	} else {
+		for (i = size; i != 0; i--)
+			dst[i-1] = src[i-1];
+	}
+	return dstptr;
+}
+
+int memcmp(const void* aptr, const void* bptr, size_t size) {
+	const unsigned char* a = (const unsigned char*) aptr;
+	const unsigned char* b = (const unsigned char*) bptr;
+	size_t i;
+	for (i = 0; i < size; i++) {
+		if (a[i] < b[i])
+			return -1;
+		else if (b[i] < a[i])
+			return 1;
+	}
+	return 0;
+}
+
+void* memset(void* bufptr, int value, size_t size) {
+	unsigned char* buf = (unsigned char*) bufptr;
+	size_t i;
+	for (i = 0; i < size; i++)
+		buf[i] = (unsigned char) value;
+	return bufptr;
+}
+
+void* memcpy(void* dstptr, const void* srcptr, size_t size) {
+	unsigned char* dst = (unsigned char*) dstptr;
+	const unsigned char* src = (const unsigned char*) srcptr;
+	size_t i;
+	for (i = 0; i < size; i++)
+		dst[i] = src[i];
+	return dstptr;
+}
